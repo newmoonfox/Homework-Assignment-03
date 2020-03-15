2e03d424d714f37b0a0569a92830f337fc1dfb89
newmoonfox <vbm5020@gmail.com>
2020-03-14 22:30:32 -0400

    Button click didn't work, redefined variables, trouble-shoot code to figure out what's breaking it

--- script.js
+++ script.js
@@ -1,19 +1,20 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 // -----------------------------------------------------------------------------------
+
 // Creating a function to generate a password and then adding variables within function for passwords
 function generatePassword() {
   var numberSet = '0123456789';
-  var specialCharacters = '!@#$%^&*?';
-  var upperCaseCharacters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-  var lowerCaseCharacters = 'abcdefghijklmnopqrstuvwxyz';
+  var specialSymbols = '!@#$%^&*?';
+  var upperCaseAlpha = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  var lowerCaseAlpha = 'abcdefghijklmnopqrstuvwxyz';
   var characterSet = '';
   var password = '';
-  // Creating Password lengths
 
-  var choices = prompt("How many characters would you like in your password?");
+  // Creating Password lengths
+  var choices = prompt("How many characters would you like in your password? :D");
   parseInt(choices, 15)
-  if (choices > 128 || choices < 8) {
+  if (choices < 8 || choices > 128) {
     alert("Password length should be 8-128 characters. Please try again. :(");
     prompt("How many characters do you want in your password?");
   }
@@ -29,24 +30,26 @@ function generatePassword() {
   }
   var special = confirm('Would you like special characters in your passwork?');
   if (special === true) {
-    characterSet = specialCharacters + characterSet;
+    characterSet = specialSymbols + characterSet;
   }
 
   var upperCase = confirm('Would you like uppercase letters in your password?');
   if (upperCase === true) {
-    characterSet = upperCaseCharacters + characterSet;
+    characterSet = upperCaseAlpha + characterSet;
   }
 
   var lowerCase = confirm('Would you like lowercase letters in your password?');
   if (lowerCase === true) {
-    characterSet = lowerCaseCharacters + characterSet;
+    characterSet = lowerCaseAlpha + characterSet;
   }
-  // Using a forloop to create random passwords
+  // Using a forloop and math.floor to create random passwords
   for (var i = 0; i < choices; i++) {
     var password = password + characterSet[Math.floor(Math.random() * characterSet.length)];
   }
   return password;
 }
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -54,6 +57,3 @@ function writePassword() {
   passwordText.value = password;
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
-  }
